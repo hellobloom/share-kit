2925a8e9ce0f4c8a5622f0d4f5f62a7ddfae93d3
Eddie Hedges <eddiehedges@gmail.com>
2019-01-10 12:12:07 -0600

    Add `getMobileUrl` to use with `react-device-detect`

--- src/RequestQRCode.tsx
+++ src/RequestQRCode.tsx
@@ -3,6 +3,8 @@ import * as React from 'react'
 import {RequestData, Options} from './types'
 import {generateRequestQRCode} from './generateRequestQRCode'
 
+import {isMobile} from 'react-device-detect'
+
 type RequestQRCodeProps = Partial<Options> & {
   requestData: RequestData
 }
@@ -15,6 +17,9 @@ class RequestQRCode extends React.Component<RequestQRCodeProps> {
 
     this.canvasRef = React.createRef()
   }
+
+  private getMobileUrl = (requestData: RequestData) =>
+    `bloom://share?request=${window.btoa(JSON.stringify(requestData))}`
 
   private generate = () => {
     if (this.canvasRef.current) {
@@ -32,7 +37,11 @@ class RequestQRCode extends React.Component<RequestQRCodeProps> {
   }
 
   render() {
-    return <canvas ref={this.canvasRef} />
+    return isMobile ? (
+      <a href={this.getMobileUrl(this.props.requestData)}>Tap to share!</a>
+    ) : (
+      <canvas ref={this.canvasRef} />
+    )
   }
 }
 
