ea3fafc53d461d1e5e97ae7e452cb753e49705bf
Jace <jacehensley@gmail.com>
2020-01-03 10:55:27 -0700

    Use our published eslint and prettier configs

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,1 @@
-module.exports = {
-  semi: false,
-  singleQuote: true,
-  printWidth: 140,
-  trailingComma: "all",
-  jsxBracketSameLine: false,
-  bracketSpacing: false
-};
+module.exports = require('@bloomprotocol/prettier-config')
